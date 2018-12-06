3fd5b3475786a45008752e3ea9422911c0911d8a
Mo Hon Sing <h.s.mo@oocl.com>
2018-12-06 14:32:54 +0800

    feat: add uers pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Link, Switch} from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import AboutPage from "./components/AboutPage.js"
 import HomePage from "./components/HomePage.js"
 import NotFoundPage from './components/NotFoundPage';
@@ -8,22 +8,32 @@ import NotFoundPage from './components/NotFoundPage';
 class App extends Component {
   render() {
     return (
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-          <hr />
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">user</Link>
+          </li>
+          <li>
+            <Link to="/users/1">user1</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/users/:userid" component={({match}) => {
+            return <h1>hello user,  {match.params.userid}</h1>
+          }}/>
+          <Route path="/users/" component={ () =><h1>hello users</h1>}/>
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
     );
   }
 }
