9f0a5e007592b91d0d8923ee2a927c57ff737580
Mo Hon Sing <h.s.mo@oocl.com>
2018-12-06 14:45:08 +0800

    feat: add go back on aboutpage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 export default class AboutPage extends Component {
-  render() {
-    return (
-      <div>
-        <h2>About</h2>
-      </div>
-    )
-  }
+    onCLick = () => {
+        const { history } = this.props
+        //shistory.goBack()
+        history.go(-1)
+    }
+    render() {
+        return (
+            <div>
+                <h2>About</h2>
+                <button onClick ={this.onClick}>go back</button>
+            </div>
+        )
+    }
 }
